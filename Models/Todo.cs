14964baf6de45be42cd4e11391cd5eef0c999e8f
maxmalato-dell-3567 <max.malato@hotmail.com>
2025-06-23 17:05:20 -0300

    acionando o modal e condicao do botão de exclusao por completo

--- Models/Todo.cs
+++ Models/Todo.cs
@@ -6,17 +6,17 @@ public class Todo
 {
     public int Id { get; set; }
     
-    [Required(ErrorMessage = "O {0} é obrigatório.")]
-    [StringLength(100, ErrorMessage = "O {0} deve ter no máximo 100 caracteres.")]
+    [Required(ErrorMessage = "{0} é obrigatório.")]
+    [StringLength(100, ErrorMessage = "Deve ter no máximo 100 caracteres.")]
     [Display(Name = "Título")]
     public string Title { get; set; } = string.Empty;
     
-    [Required(ErrorMessage = "A {0} é obrigatória.")]
-    [StringLength(300, ErrorMessage = "A {0} deve ter no máximo 300 caracteres.")]
+    [Required(ErrorMessage = "{0} é obrigatória.")]
+    [StringLength(300, ErrorMessage = "Deve ter no máximo 300 caracteres.")]
     [Display(Name = "Descrição")]
     public string Description { get; set; } = string.Empty;
-    
-    [Display(Name = "Concluída")]
+
+    [Display(Name = "Concluído")]
     public bool IsDone { get; set; } = false;
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 }
