65355b620844226bde0b3a0602a0b57cdf3294c9
maxmalato-dell-3567 <max.malato@hotmail.com>
2025-06-23 10:30:14 -0300

    adicionando validacao de dados

--- Models/Todo.cs
+++ Models/Todo.cs
@@ -1,10 +1,22 @@
-﻿namespace Tasks.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tasks.Models;
 
 public class Todo
 {
     public int Id { get; set; }
+    
+    [Required(ErrorMessage = "O {0} é obrigatório.")]
+    [StringLength(100, ErrorMessage = "O {0} deve ter no máximo 100 caracteres.")]
+    [Display(Name = "Título")]
     public string Title { get; set; } = string.Empty;
+    
+    [Required(ErrorMessage = "A {0} é obrigatória.")]
+    [StringLength(300, ErrorMessage = "A {0} deve ter no máximo 300 caracteres.")]
+    [Display(Name = "Descrição")]
     public string Description { get; set; } = string.Empty;
+    
+    [Display(Name = "Concluída")]
     public bool IsDone { get; set; } = false;
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 }
